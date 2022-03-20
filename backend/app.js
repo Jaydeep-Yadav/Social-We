58fddd7086224ea66b7339cb3fc238f3b4e40c0b
Jaydeep Yadav <yadavjay374@gmail.com>
2022-03-20 18:39:22 +0530

    Hosting

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,6 @@ const user = require("./routes/user");
 app.use("/api/v1", post);
 app.use("/api/v1", user);
 
-
 app.use(express.static(path.join(__dirname,"../frontend/build")));
 app.get("*",(req,res)=>{
   res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
